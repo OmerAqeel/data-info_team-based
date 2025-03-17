428a7e8d076f35ddc673a16b00a05f304cbb58f9
OmerAqeel <omrraqeel@gmail.com>
2025-03-17 10:54:18 +0000

    added some changes for the server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors()); // Allows frontend to access the backend
 app.use(express.json()); // Middleware to parse JSON
