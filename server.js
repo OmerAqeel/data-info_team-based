c68cac21539f1086c83f481030dc6346a15a2fc9
OmerAqeel <omrraqeel@gmail.com>
2025-03-21 12:05:03 +0000

    updated the port in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000; // Use Render's provided PORT
 
 app.use(cors()); // Allows frontend to access the backend
 app.use(express.json()); // Middleware to parse JSON
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
